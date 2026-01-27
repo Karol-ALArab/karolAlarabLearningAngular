4bb61cddab37f4893a9bb67b4f69dced89ca0b88
Karol <Karol@test>
2026-01-27 18:37:25 -0500

    Step 4: Component1-2

---
+++ src/app/equipment-list-item/equipment-list-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-list-item',
+  imports: [],
+  templateUrl: './equipment-list-item.html',
+  styleUrl: './equipment-list-item.css',
+})
+export class EquipmentListItem {
+
+}
---
+++ src/app/equipment-list/equipment-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-list',
+  imports: [],
+  templateUrl: './equipment-list.html',
+  styleUrl: './equipment-list.css',
+})
+export class EquipmentList {
+
+}
