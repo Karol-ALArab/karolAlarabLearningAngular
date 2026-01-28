32a20c20b82cb398d37d1e95c7009ce2019d31ca
Karol <Karol@test>
2026-01-27 19:35:17 -0500

    fixup! Step 5-8: changing app.html and adding content tags

--- src/app/equipment-list/equipment-list.ts
+++ src/app/equipment-list/equipment-list.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {Items} from '../shared-models/items';
 import {EquipmentListItem} from '../equipment-list-item/equipment-list-item';
+import {NgFor} from '@angular/common';
+
 
 @Component({
   selector: 'app-equipment-list',
   imports: [
-    EquipmentListItem
+    EquipmentListItem,
+  NgFor
   ],
   templateUrl: './equipment-list.html',
   styleUrl: './equipment-list.css',
