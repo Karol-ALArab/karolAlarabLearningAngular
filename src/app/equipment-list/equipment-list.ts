b283c7681b9f503b1fc9f842be2a7d84682b4edf
Karol <Karol@test>
2026-02-06 14:35:02 -0500

    Assigment 4: Added equipment service

--- src/app/equipment-list/equipment-list.ts
+++ src/app/equipment-list/equipment-list.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Items} from '../shared-models/items';
 import {EquipmentListItem} from '../equipment-list-item/equipment-list-item';
 import {NgFor} from '@angular/common';
+import {EquipmentService} from '../services/equipment';
 
 
 @Component({
@@ -13,12 +14,16 @@ import {NgFor} from '@angular/common';
   templateUrl: './equipment-list.html',
   styleUrl: './equipment-list.css',
 })
-export class EquipmentList {
+export class EquipmentList implements OnInit {
   items: Items[] = [];
+
+
+  constructor(private equipmentService: EquipmentService) { }
+
+  ngOnInit(): void {
+        throw new Error("Method not implemented.");
+    }
 
 
 
 }
-
-
-
--- src/app/services/equipment.ts
+++ src/app/services/equipment.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Items } from '../shared-models/items';
+import { MOCK_EQUIPMENT } from '../data/mock-equipment';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Equipment {
-  
+export class EquipmentService {
+  private equipment: Items[] = MOCK_EQUIPMENT;
+
+  constructor() { }
+
+
+  getEquipment(): Observable<Items[]> {
+    return of(this.equipment);
+  }
 }
