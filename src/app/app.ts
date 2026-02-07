d8fdcca407bb6c4d49eb20c9fac58af891bc56d8
Karol <Karol@test>
2026-02-06 21:28:20 -0500

    Assigment 4: Added Services to appComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,27 +1,32 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import {Items} from './shared-models/items';
 import {EquipmentList} from './equipment-list/equipment-list';
+import {EquipmentListItem} from './equipment-list-item/equipment-list-item';
+import {EquipmentService} from './services/equipment';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, EquipmentList],
+  imports: [CommonModule, EquipmentList, EquipmentListItem],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('karolAlarabLearningAngular');
   studentName: string = 'Karol Alarab';
   courseCode: string = 'MAD 307';
+  selectedEquipment?: Items;
 
-  items: Items[] = [
-    { id: 1, name: 'Soccer Ball', price: 30,  description: 'Official size 5 match ball', category: 'Equipment' },
-    { id: 2, name: 'Soccer Cleats', price: 120,description: 'Clean Light weight cleats', category: 'Footwear'  },
-    { id: 3, name: 'Goalkeeper Gloves', price: 60, description: 'Enhanced gloves for goalies', category: 'Gear' },
-    { id: 4, name: 'Team Jersey', price: 90, description: 'Official Real Madrid home jersey', category: 'Apparel' },
-    { id: 5, name: 'Shin Guards', price: 25, description: 'Lightweight shin guards',category: 'Protection'  },
-    { id: 6, name: 'Training Cones', price: 20, description: 'Set of 12 training cones', category: 'Training'  }
-  ];
+
+  constructor(private equipmentService: EquipmentService) { }
+
+  ngOnInit() {
+    this.equipmentService.getEquipmentById(1).subscribe({
+      next: (data: Items | undefined) => this.selectedEquipment = data,
+      error: err => console.error("Error ", err),
+      complete: () => console.log("Success!")
+    });
+  }
 }
