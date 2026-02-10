f79fd7cb80cdb0e0272e755b212a6255a908feb1
Karol <Karol@test>
2026-02-10 17:17:19 -0500

    Assigment 5: Defining list of routes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {Routes} from '@angular/router';
+import {EquipmentList} from './app/equipment-list/equipment-list';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/equipment', pathMatch: 'full' },
+  { path: 'equipment', component: EquipmentList },
+];
+
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
