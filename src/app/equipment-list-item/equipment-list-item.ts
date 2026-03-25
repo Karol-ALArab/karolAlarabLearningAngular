f74189cf734653dcf32b7a8658a8eab6117e0bd1
Karol <Karol@test>
2026-03-25 02:51:21 -0400

    Assigment 8 step: 6 adding pipes

--- src/app/equipment-list-item/equipment-list-item.ts
+++ src/app/equipment-list-item/equipment-list-item.ts
@@ -1,11 +1,12 @@
 import {Component, Input, } from '@angular/core';
 import {Items} from '../shared-models/items';
-import {NgIf,NgOptimizedImage} from '@angular/common';
+import {CurrencyPipe, DatePipe, NgIf, NgOptimizedImage, UpperCasePipe} from '@angular/common';
+import {EquipmentLabel} from '../pipes/equipment-label';
 
 @Component({
   selector: 'app-equipment-list-item',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, UpperCasePipe, CurrencyPipe, DatePipe, EquipmentLabel],
   templateUrl: './equipment-list-item.html',
   styleUrl: './equipment-list-item.css',
 })
