a68fb15a281061cdc7a3ee670f35ef5d251d6c27
Karol <Karol@test>
2026-04-07 20:10:27 -0400

    Assigment 10: completed final commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule,
+    MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
